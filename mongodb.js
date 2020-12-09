bcdcad551036965fba6abd7c9ebc287e7c48b83d
Ariel Kuźmiński <ariel.kuzminski@gmail.com>
2020-12-10 00:37:45 +0100

    Destructurize object

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,10 @@
 // CRUD
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
