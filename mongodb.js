199816d27dab7fd53532b6bc191dbe02c3d531f2
Ariel Kuźmiński <ariel.kuzminski@gmail.com>
2020-12-09 01:31:16 +0100

    Connect to DB and create tasks

---
+++ mongodb.js
@@ -0,0 +1,37 @@
+// CRUD
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('task').insertMany([
+        {
+            description: 'Go to sleep',
+            completed: false,
+        },
+        {
+            description: 'Finish mongoDb tutorial',
+            completed: false,
+        },
+        {
+            description: 'Say goodbye to Paula',
+            completed: true,
+        },
+    ], (error, result) => {
+        if (error) {
+            return console.log('Failed to add tasks');
+        }
+        console.log(result.ops);
+    })
+
+
+});
