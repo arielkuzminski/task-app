ae8c0a25ce3f864caea807a86b3f165e3ffaa1a4
Ariel <ariel.kuzminski@gmail.com>
2021-09-27 20:16:38 +0200

    Get Tasks and task by id

--- src/index.js
+++ src/index.js
@@ -43,6 +43,25 @@ app.post('/tasks', (req, res) => {
     }).catch((error) => {
         res.status(400).send(error.message);
     })
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((error) => {
+        res.status(500).send();
+    });
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        if (!task) {
+            res.status(404).send();
+        }
+        res.send(task);
+    }).catch((error) => {
+        res.status(500).send();
+    });
 })
 
 app.listen(port, () => {
