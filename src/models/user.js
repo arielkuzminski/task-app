90a3820d2a245d72204c664dec57a8357c5098d1
Ariel <ariel.kuzminski@gmail.com>
2021-10-13 23:41:29 +0200

    Generate and store user token when login and register

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,11 +40,27 @@ const userSchema = new mongoose.Schema({
                 throw new Error('cant use that password');
             }
         }
-    }
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true,
+            }
+        }
+    ]
 });
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+}
+
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
     if (!user) {
         throw new Error('Unable to login');
@@ -59,7 +76,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
 };
 
 // Hash the plain text password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
     const user = this;
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
